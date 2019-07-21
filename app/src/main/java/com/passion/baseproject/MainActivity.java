3547508805819b29e6656d02398ca79054555be8
yi.zhang <yi.zhang@jing-tong.com>
2019-07-21 17:21:11 +0800

    [feature]项目升级到Androidx

--- app/src/main/java/com/passion/baseproject/MainActivity.java
+++ app/src/main/java/com/passion/baseproject/MainActivity.java
@@ -1,13 +1,27 @@
 package com.passion.baseproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button)
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onViewClicked() {
     }
 }
